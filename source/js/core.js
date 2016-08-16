2b515836a9b28366a92ac413136aa1fbb2750a03
Lilly Dorgan <ldorgan@squiz.co.nz>
2016-08-17 09:16:10 +1200

    details page changes

--- source/js/core.js
+++ source/js/core.js
@@ -1,4 +1,4 @@
 //@prepros-append ../bower_components/jquery/jquery.min.js
 //@prepros-append lib/slick.min.js
 //@prepros-append lib/aos.js
-//@prepros-append lib/bootstrap-select.min.js
+//@prepros-append lib/multiselect.js
